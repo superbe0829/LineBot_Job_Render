3220f6aec01365440ac2ff6a8146e0e40aba17e4
superbe0829 <superbe.chang@gmail.com>
2025-01-17 08:48:01 +0800

    Update LineBot_Job_Render_1140115.py

--- LineBot_Job_Render_1140115.py
+++ LineBot_Job_Render_1140115.py
@@ -215,17 +215,18 @@ def handle_message(event):
                 original_content_url="https://drive.google.com/uc?export=view&id=1WuWb4CVkn1cIHBiD83Jp0bMzIRHlZIZZ",
                 preview_image_url="https://drive.google.com/uc?export=view&id=1WuWb4CVkn1cIHBiD83Jp0bMzIRHlZIZZ"
             )
-            # image_message2 = ImageSendMessage(
-            #     original_content_url="https://drive.google.com/uc?export=view&id=1ZxbpiEbwMiZ1vmMN8szHZj8C119AtQVZ",
-            #     preview_image_url="https://drive.google.com/uc?export=view&id=1ZxbpiEbwMiZ1vmMN8szHZj8C119AtQVZ"
-            # )
+            image_message2 = ImageSendMessage(
+                original_content_url="https://drive.google.com/uc?export=view&id=1ZxbpiEbwMiZ1vmMN8szHZj8C119AtQVZ",
+                preview_image_url="https://drive.google.com/uc?export=view&id=1ZxbpiEbwMiZ1vmMN8szHZj8C119AtQVZ"
+            )
             video_message = VideoSendMessage(
                 original_content_url="https://drive.google.com/uc?export=view&id=1ObbuUjvqK8lDVsymER0vYfyVZl049yee",  # 替換為你的影片 URL
                 preview_image_url="https://drive.google.com/uc?export=view&id=1Z5HwsY-nrzu6Fn6_CcEsDsIrYVhylQQf"  # 必須提供
             )
 
             # result_message = [TextSendMessage(text="人資宣導資料如下："), image_message1, image_message2]  # 這裡是多個訊息，應該用列表
-            result_message = [TextSendMessage(text="人資宣導資料如下："), image_message1, video_message]  # 這裡是多個訊息，應該用列表
+            # result_message = [TextSendMessage(text="人資宣導資料如下："), image_message1, video_message]  # 這裡是多個訊息，應該用列表
+            result_message = [TextSendMessage(text="以下是DM宣導："), image_message1, image_message2, TextSendMessage(text="以下是短片宣導："), video_message]  # 這裡是多個訊息，應該用列表
             
             # 使用 reply_message 回覆結果
             line_bot_api.reply_message(event.reply_token, result_message)
@@ -246,7 +247,7 @@ def process_request(user_id, user_message):
         if "@徵才活動" in user_message:
             events = fetch_job_events()
             if events:
-                reply_message = "以下是近期10場最新徵才活動：\n" + "\n\n".join(
+                reply_message = "以下是最近10場徵才活動資訊：\n" + "\n\n".join(
                     [f"{event['index']}. {event['name']}\n詳細資訊：{event['link']}" for event in events[:10]]
                 )
             else:
